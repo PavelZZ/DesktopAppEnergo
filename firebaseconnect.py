07ea7e7d908e2977409d4c2c1e1dae8417e78b86
Paul <skinyman1964@gmail.com>
2022-03-28 23:20:13 +0500

    Локальная БД

--- firebaseconnect.py
+++ firebaseconnect.py
@@ -1,6 +1,6 @@
 import pyrebase
 from PyQt5.QtWidgets import QMainWindow
-
+import sqlite3
 from  Ui_ChooseExecutors import Ui_MainWindow as ChooseExecutors
 from secondScreen import SecondScreen
 from Ui_AuthWindow import Ui_MainWindow
@@ -11,22 +11,28 @@ firebaseconfig = {'apiKey': "AIzaSyDQeQ_YV0ZVeLW--dzDt6XntEwcCEGwTrg",
                   'storageBucket': "energotemp-9b8c9.appspot.com",
                   'messagingSenderId': "622369709896",
                   'appId': "1:622369709896:web:5b1121856ffdff3a4e9d7d"}
-
 fireBaseApp= pyrebase.initialize_app(firebaseconfig)
 database = fireBaseApp.database()
 
 class AuthWindow (Ui_MainWindow):
     # Own methods
     def authorize(self, login):
-        allUsers = database.child("users").shallow().get()
-        userNames = allUsers.val()
-        # if login in userNames:
-        self.label_3.setText("Авторизация прошла успешно!")
-        self.chooseExecutors = QMainWindow()
-        self.chooseExecutorsUi= SecondScreen()
-        self.chooseExecutorsUi.setupUi(self.chooseExecutors)
-        self.chooseExecutorsUi.initEventListeners()
-        self.chooseExecutors.show()
+        # TODO: Сделать вход по паролю
+        # Переделать чуть код после изменения данных на firebase
+        con = sqlite3.connect("users.db")
+        cur = con.cursor()
+        cur.execute("SELECT user FROM USERSTABLE WHERE user LIKE '" + login + "'")
+        rows = cur.fetchall()
+        print(rows)
+        cur.close()
+        print("check2")
+        if len(rows) > 0:
+            self.label_3.setText("Авторизация прошла успешно!")
+            self.chooseExecutors = QMainWindow()
+            self.chooseExecutorsUi= SecondScreen()
+            self.chooseExecutorsUi.setupUi(self.chooseExecutors)
+            self.chooseExecutorsUi.initEventListeners()
+            self.chooseExecutors.show()
         # else:
         #     self.label_3.setText("Неверный логин, попробуйте снова")
 
@@ -34,7 +40,22 @@ class AuthWindow (Ui_MainWindow):
         self.pushButton.clicked.connect(
             lambda: self.authorize(self.lineEdit.text()))
         self.lineEdit.textChanged.connect(self.clearAuthStatusLabel)
+        self.update_btn.mousePressEvent = self.updateDB
 
     def clearAuthStatusLabel(self):
         self.label_3.clear()
 
+    def updateDB(self,event):
+        con = sqlite3.connect("users.db")
+        cur = con.cursor()
+        print("check")
+        allUsers = database.child("users").get()
+        print(allUsers)
+        # Временно
+        cur.execute("DROP TABLE IF EXISTS USERSTABLE")
+        cur.execute("CREATE TABLE USERSTABLE (id INTEGER PRIMARY KEY , user TEXT, password TEXT);")
+        for i in range(len(allUsers.val())):
+            cur.execute(f"INSERT INTO USERSTABLE VALUES ('{i}','{allUsers[i].key()}','string')")
+        con.commit()
+        con.close()
+        print("Done")
